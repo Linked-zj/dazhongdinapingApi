5379fa9ef44788a102e3fe7d96579f9fa929b942
Linked <2323686277@qq.com>
2020-03-29 20:25:30 +0800

    店铺列表修改

---
+++ src/main/java/com/linked/dazhongdianpingapi/business/pojo/vo/shop/ShopListVO.java
@@ -0,0 +1,15 @@
+package com.linked.dazhongdianpingapi.business.pojo.vo.shop;
+
+import lombok.Data;
+
+/**
+ * @author Linked
+ * @date 2020/3/28 23:06
+ */
+@Data
+public class ShopListVO {
+
+    private Integer categoryId;
+
+
+}
