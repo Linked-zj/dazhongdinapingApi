07408ff0d11e10e50fb88c4416734ccd2dfe8db5
Linked <2323686277@qq.com>
2020-04-12 21:52:12 +0800

    店铺详情接口

--- src/main/java/com/linked/dazhongdianpingapi/business/service/shop/ShopService.java
+++ src/main/java/com/linked/dazhongdianpingapi/business/service/shop/ShopService.java
@@ -1,7 +1,11 @@
 package com.linked.dazhongdianpingapi.business.service.shop;
 
+import com.linked.dazhongdianpingapi.business.dao.CommentDao;
 import com.linked.dazhongdianpingapi.business.dao.ShopDao;
+import com.linked.dazhongdianpingapi.business.pojo.dto.ShopDetailDTO;
+import com.linked.dazhongdianpingapi.business.pojo.po.Comment;
 import com.linked.dazhongdianpingapi.business.pojo.po.Shop;
+import com.linked.dazhongdianpingapi.business.pojo.vo.shop.ShopDetailVO;
 import com.linked.dazhongdianpingapi.business.pojo.vo.shop.ShopListVO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,7 +23,22 @@ public class ShopService {
     @Autowired
     private ShopDao shopDao;
 
+    @Autowired
+    private CommentDao commentDao;
+
     public List<Shop> getShopList(ShopListVO listVO) {
         return shopDao.getShopList(listVO);
+    }
+
+    public ShopDetailDTO getShopDetail(ShopDetailVO detailVO) {
+        ShopDetailDTO detailDTO = new ShopDetailDTO();
+        Shop shop = shopDao.selectByPrimaryKey(detailVO.getShopId());
+        Example example = new Example(Comment.class);
+        Example.Criteria criteria = example.createCriteria();
+        criteria.andEqualTo("shopCommentedId", detailVO.getShopId());
+        List<Comment> comments = commentDao.selectByExample(example);
+        detailDTO.setComments(comments);
+        detailDTO.setShop(shop);
+        return detailDTO;
     }
 }
