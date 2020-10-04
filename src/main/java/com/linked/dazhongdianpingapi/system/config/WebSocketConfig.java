6c8c16dda12e34d9c73322df96debd1b6d3f3bba
Linked <2323686277@qq.com>
2020-10-04 11:39:42 +0800

    打包

--- src/main/java/com/linked/dazhongdianpingapi/system/config/WebSocketConfig.java
+++ src/main/java/com/linked/dazhongdianpingapi/system/config/WebSocketConfig.java
@@ -11,10 +11,10 @@ import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 @Configuration
 public class WebSocketConfig {
 
-    @Bean
-    public ServerEndpointExporter serverEndpointExporter(){
-        return new ServerEndpointExporter();
-    }
+//    @Bean
+//    public ServerEndpointExporter serverEndpointExporter(){
+//        return new ServerEndpointExporter();
+//    }
 
 
 
